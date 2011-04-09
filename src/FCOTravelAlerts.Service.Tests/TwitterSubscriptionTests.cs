56e2130e5107a26b98652195bcffece2ceaf4f1f
tony <tony.to@7digital.com>
2011-04-09 12:50:11 +0100

    peoject shell#

---
+++ src/FCOTravelAlerts.Service.Tests/TwitterSubscriptionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FCOTravelAlerts.Service.Tests
+{
+    [TestFixture]
+    public class TwitterSubscriptionTests
+    {
+
+        [Test]
+        public void should_be_able_to_connect_to_the_FCO_twitter_feed()
+        {
+            Assert.That(feed,Is.StringContaining());
+        }
+    }
+}
