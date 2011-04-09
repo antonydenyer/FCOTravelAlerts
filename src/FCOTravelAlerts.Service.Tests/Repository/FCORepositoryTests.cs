d2871c00bf8ee5d4b5e8053518f39194ce892629
tony <tony.to@7digital.com>
2011-04-09 16:30:52 +0100

    email notification

--- src/FCOTravelAlerts.Service.Tests/Repository/FCORepositoryTests.cs
+++ src/FCOTravelAlerts.Service.Tests/Repository/FCORepositoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using FCOTravelAlerts.Service.Entity;
 using FCOTravelAlerts.Service.Repository;
 using NUnit.Framework;
 using Rhino.Mocks;
@@ -9,16 +10,22 @@ namespace FCOTravelAlerts.Service.Tests.Repository
     [TestFixture]
     public class FCORepositoryTests
     {
+        private FCORepository _fcoRepository;
 
-        [Test]
-        public void Can_map_response_from_requst()
+        [SetUp]
+        private void Setup()
         {
             var webRequest = MockRepository.GenerateStub<IXmlWebRequest>();
             webRequest.Stub(x => x.Get(Arg<string>.Is.Anything))
                 .Return(FeedData.Data);
 
-            var fcoRepository = new FCORepository(webRequest);
-            var items = fcoRepository.Get();
+            _fcoRepository = new FCORepository(webRequest);
+        }
+
+        [Test]
+        public void Can_map_response_from_requst()
+        {
+            var items = _fcoRepository.Get();
 
             Assert.That(items.Count(),Is.EqualTo(20));
             Assert.That(items.First().DatePublished.Date, Is.EqualTo(new DateTime(2011,04,09).Date));
@@ -29,6 +36,13 @@ namespace FCOTravelAlerts.Service.Tests.Repository
         {
             var items = new FCORepository(new XmlWebRequest()).Get();
             items.ToList().ForEach(Console.WriteLine);
+        }
+
+        [Test]
+        public void can_parse_hash_tag()
+        {
+            var hashtag = new HashTagParser().Parse(new Item("#libya blah", DateTime.Now));
+            Assert.That(hashtag, Is.EqualTo("libya"));
         }
     }
 }
