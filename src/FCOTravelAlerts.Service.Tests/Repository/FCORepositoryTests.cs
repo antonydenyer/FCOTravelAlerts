6950b74456ec4948bb672e5cb01fb0fb80739ef3
tony <tony.to@7digital.com>
2011-04-09 14:19:27 +0100

    move to the correct place

--- src/FCOTravelAlerts.Service.Tests/Repository/FCORepositoryTests.cs
+++ src/FCOTravelAlerts.Service.Tests/Repository/FCORepositoryTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Xml;
-using System.Xml.Linq;
+using FCOTravelAlerts.Service.Repository;
 using NUnit.Framework;
 using Rhino.Mocks;
 
@@ -25,59 +23,5 @@ namespace FCOTravelAlerts.Service.Tests.Repository
             Assert.That(items.Count(),Is.EqualTo(20));
             Assert.That(items.First().DatePublished.Date, Is.EqualTo(new DateTime(2011,04,09).Date));
         }
-    }
-
-    public class FCORepository
-    {
-        private readonly IXmlWebRequest _webRequest;
-
-        private const string FEED = "http://pipes.yahoo.com/pipes/pipe.run?_id=ac45e9eb9b0174a4e53f23c4c9903c3f&_render=rss&statustitle=logo&username=fcotravel";
-       
-        public FCORepository(IXmlWebRequest webRequest)
-        {
-            _webRequest = webRequest;
-        }
-
-        public IEnumerable<Item> Get()
-        {
-            XmlDocument data = _webRequest.Get(FEED);
-            var items = new List<Item>();
-            foreach (XmlNode item in data.SelectNodes("//item"))
-            {
-                var title = item.SelectSingleNode("title").InnerText;
-                var datePublished = DateTime.Parse(item.SelectSingleNode("pubDate").InnerText);
-                items.Add(new Item(title,datePublished));
-            }
-
-            
-            return items;
-        }
-    }
-
-    public class Item
-    {
-        private readonly string _title;
-        private readonly DateTime _datePublished;
-
-        public Item(string title, DateTime datePublished)
-        {
-            _title = title;
-            _datePublished = datePublished;
-        }
-
-        public DateTime DatePublished
-        {
-            get { return _datePublished; }
-        }
-
-        public string Title
-        {
-            get { return _title; }
-        }
-    }
-
-    public interface IXmlWebRequest
-    {
-        XmlDocument Get(string uri);
     }
 }
